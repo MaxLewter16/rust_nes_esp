2b1f7d13a6039e0957128c47382fae17e5f34e81
MaxLewter16 <lewtermax@gmail.com>
2025-02-27 11:34:21 -0700

    added way to count opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+#![allow(unused_variables)]
+
 pub mod cpu;
 pub mod opmap;
 pub mod ppu;
 pub mod memory;
+use crate::opmap::OP_MAP;
+use crate::cpu::CPU;
+
+fn count_valid_ops() -> usize {
+    OP_MAP.iter().filter(|&&op| op as usize != CPU::noop as usize).count()
+}
 
 fn main() {
-    println!("Hello, world!");
+    let valid_opcodes = count_valid_ops();
+    println!("Number of implemented opcodes: {}", valid_opcodes);
 }
 
